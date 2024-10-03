2334ea47c93f3d6fcd6796681ba4111ea535daf9
Gopi9575 <gopipatidar9575@gmail.com>
2024-10-03 22:24:40 +0530

    new

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    private static int add(int x, int y) {
+        return x + y;
+    }
+
+    // public static void main(String[] arg) {
+        // System.out.println(add(2, 3));
+    // }
+
+
+Package Oops;
+
+public class calculator {
+    public static void main(String[] arg) {
+        System.out.println(Test.add(3, 6));
+    }
+  }
+}
